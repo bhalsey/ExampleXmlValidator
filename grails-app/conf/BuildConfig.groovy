465ef8576bce8043c76fb56072c8d14adb00c2c7
Brent Halsey <bhalsey@pearsonkt.com>
2010-05-11 14:30:31 -0600

    Made path to XmlValidator plugin relative

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,4 +1,4 @@
-grails.plugin.location."xml-validator" = "/Users/bhalsey/proj/XmlValidator"
+grails.plugin.location."xml-validator" = "../XmlValidator"
 
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
