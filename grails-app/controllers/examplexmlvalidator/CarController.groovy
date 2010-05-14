0c18026ef997c4d0f94becf1f401cff50b65dba4
Brent Halsey <mrbrent@gmail.com>
2010-05-13 23:58:11 -0600

    Added controller actions and corresponding curl scripts to test validation works with cached xml objects and xsd schema files.

--- grails-app/controllers/examplexmlvalidator/CarController.groovy
+++ grails-app/controllers/examplexmlvalidator/CarController.groovy
@@ -6,6 +6,7 @@ class CarController {
 
     def index = { }
 
+    /** pretend to add cars to something.  Doesn't perform schema validation. */
     def add = {
 	def xml = request.XML
 	int count = 0
@@ -21,7 +22,8 @@ class CarController {
 	}
     }
 
-    def addSafe = {
+    /** pretend to add cars to something.  Performs schema validation. */
+    def addValidate = {
 	try {
 	    def xml = request.XML(carRecordsSchema)
 
@@ -45,7 +47,63 @@ class CarController {
 		message("Error in xml schema: " + e.message)
 	    }
 	}
-	    
+    }
+
+    /** pretend to add cars to something.  Performs schema validation. */
+    def addValidateSchemaFile = {
+	try {
+	    def xml = request.XML("/xsd/car-records.xsd")
+
+	    // test that we actually cached our xml object
+	    xml = request.XML
+
+	    int count = 0
+	    xml.car.each { car ->
+		log.warn "Adding car of make ${car.@make}"
+		log.warn "has a record of type: ${car.record.@type} in ${car.record.text()}"
+		// add cars here
+		count++
+	    }
+
+	    render(contentType: "text/xml") {
+		message("Added ${count} cars.")
+	    }
+	}
+	catch (SAXException e) {
+	    render(contentType: "text/xml") {
+		message("Error in xml schema: " + e.message)
+	    }
+	}
+    }
+
+    /** pretend to add cars to something.  Tests that we still perform schema 
+     *  even after calling request.XML, which caches the parsed xml object.
+     */
+    def addTestCache = {
+	try {
+	    def tempXml = request.XML
+	    def xml = request.XML(carRecordsSchema)
+
+	    // test that we actually cached our xml object
+	    xml = request.XML
+
+	    int count = 0
+	    xml.car.each { car ->
+		log.warn "Adding car of make ${car.@make}"
+		log.warn "has a record of type: ${car.record.@type} in ${car.record.text()}"
+		// add cars here
+		count++
+	    }
+
+	    render(contentType: "text/xml") {
+		message("Added ${count} cars.")
+	    }
+	}
+	catch (SAXException e) {
+	    render(contentType: "text/xml") {
+		message("Error in xml schema: " + e.message)
+	    }
+	}
     }
 
     String carRecordsSchema = '''
